56974029853ff74137bab579e14b854ca60209ac
POWER USER <poweruser@Users-iMac-2.local>
2018-11-29 17:08:33 -0500

    update

--- Db.php
+++ Db.php
@@ -4,13 +4,13 @@ use PDO;
 
 class Db{
 
-	private $db;
+	private $dbh = 'mysql:host=127.0.0.1;dbname=eli9;port=8889';
 
 	public function connect()
 	{
 
 		try {
-		    $db = new PDO("mysql:host=127.0.0.1;dbname=eli9;port=8889", 'root', 'root');
+		    $db = new PDO($this->dbh, 'root', 'root');
 		    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 				return $db;
 		}
